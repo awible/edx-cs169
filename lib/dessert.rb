8fb3bf879662f882ffafce06e367e099318fae17
awible <apwible@gmail.com>
2014-02-16 05:48:02 +0000

    dessert

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,34 @@
 class Dessert
+  attr_reader :name
+  attr_writer :name
+  attr_reader :calories
+  attr_writer :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+	@calories = calories
   end
   def healthy?
-    # your code here
+    return true if calories < 200
+  	return false
   end
   def delicious?
-    # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_reader :flavor
+  attr_writer :flavor
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+	super(flavor + " jelly bean", 5)
+  end
+  def delicious?
+	if flavor == "licorice"
+	  return false
+	else
+	  return true
+	end
   end
 end
+
